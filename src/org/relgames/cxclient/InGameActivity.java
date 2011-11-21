eb969ec582b6ac81f9e644a58a49bda63bc4c763
Oleg Poleshuk <relgames@gmail.com>
2011-11-21 17:49:44 +0100

    Styles

--- src/org/relgames/cxclient/InGameActivity.java
+++ src/org/relgames/cxclient/InGameActivity.java
@@ -27,9 +27,14 @@ public class InGameActivity extends Activity {
     private String gameId;
     private String baseUrl;
 
+    private String CSS;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        CSS = Utils.streamToString(this.getResources().openRawResource(R.raw.cxgomel));
+
         setContentView(R.layout.in_game);
 
         gameId = getIntent().getStringExtra("gameId");
@@ -46,10 +51,9 @@ public class InGameActivity extends Activity {
             InGame inGame = getCxService().inGame(gameId, null, null);
             WebView html = (WebView)findViewById(R.id.inGameHtml);
 
-            String htmlStr = "<html><body>" + inGame.levelHtml + "</body></html>";
-            Log.d(TAG + " HTML", htmlStr);
-
-            html.loadDataWithBaseURL(baseUrl, htmlStr, "text/html", "UTF-8", null);
+            Log.d(TAG + " levelHtml", inGame.levelHtml);
+            String fullHtml = "<html><head><style>"+CSS+"</style></head><body>" + inGame.levelHtml + "</body></html>";
+            html.loadDataWithBaseURL(baseUrl, fullHtml, "text/html", "UTF-8", null);
         } catch (CxServiceException e) {
             error(e, this);
         }
--- src/org/relgames/cxclient/Utils.java
+++ src/org/relgames/cxclient/Utils.java
@@ -6,10 +6,14 @@ import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.io.*;
+
 /**
  * @author Oleg Poleshuk
  */
 public class Utils {
+    private static final String TAG = "Utils";
+
     public static void error(Exception e, Context c) {
         Log.e(c.getClass().getSimpleName(), "Error", e);
 
@@ -21,14 +25,39 @@ public class Utils {
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
         String baseUrl = sp.getString("url", "");
 
-        if (! (baseUrl.startsWith("http://") || baseUrl.startsWith("https://")) ) {
+        if (!(baseUrl.startsWith("http://") || baseUrl.startsWith("https://"))) {
             baseUrl = "http://" + baseUrl;
         }
 
         if (baseUrl.endsWith("/")) {
-            baseUrl = baseUrl.substring(0, baseUrl.length()-1);
+            baseUrl = baseUrl.substring(0, baseUrl.length() - 1);
         }
 
         return baseUrl;
+    }
+
+    public static String streamToString(InputStream inputStream) {
+        if (inputStream != null) {
+
+            try {
+                Writer writer = new StringWriter();
+                char[] buffer = new char[1024];
+                try {
+                    Reader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+                    int n;
+                    while ((n = reader.read(buffer)) != -1) {
+                        writer.write(buffer, 0, n);
+                    }
+                } finally {
+                    inputStream.close();
+                }
+                return writer.toString();
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            return "";
+        }
     }
 }
